f1a4e61b683cd8eb6121074b67aed021896ac249
Pedro Abreu <pedro.abreu@ua.pt>
2019-12-05 22:05:08 +0000

    new hash and main

--- MPEI/src/project/Hash.java
+++ MPEI/src/project/Hash.java
@@ -13,5 +13,20 @@ public class Hash {
 		}
 		return hash;
 	}
+	// variant CRC, implemented from the slides with our variant
+	public static int renewedHash(String content) {
+		int ki = 1431655765;
+		int h = 0;
+		for(int i = 0; i < content.length(); i++) {
+			h += (content.codePointAt(i)*ki);
+		}
+		//int h = content.hashCode(); //tem um senão!!
+		int highorder = h & 0xf8000000;
+		h = h << 5;
+		h = h ^ (highorder >> 27);
+		h = h ^ ki;
+		//System.out.println("h: "+h);
+		return Math.abs(h);
+	}
 
 }
